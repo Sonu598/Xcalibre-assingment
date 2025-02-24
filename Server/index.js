390318b6f610c571a6d2bbd90c2be0e431563e9c
Isjupriya Rath <ishupriyarath@gmail.com>
2025-02-24 22:02:54 +0530

    Added

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const Router = require("./routes/route");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use("/hospitalAi", Router);
 

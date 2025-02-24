eae92a8f62e328ed25f0c597ab90242ccd7bbdfe
Isjupriya Rath <ishupriyarath@gmail.com>
2025-02-24 22:29:51 +0530

    Added

--- Server/routes/route.js
+++ Server/routes/route.js
@@ -18,7 +18,7 @@ Router.post("/query", async (req, res) => {
     queryCache.set(naturalQuery, rows);
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ Error: error.message });
+    res.status(500).json({ Error_: error.message });
   }
 });
 

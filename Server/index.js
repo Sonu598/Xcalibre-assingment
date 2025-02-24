6e541c6a526e40beff2e971135c86a73cb5750e2
Isjupriya Rath <ishupriyarath@gmail.com>
2025-02-24 22:57:48 +0530

    Added

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const Router = require("./routes/route");
 const app = express();
 require("dotenv").config();
-app.set("trust proxy", 1); // Trust the first proxy (Render)
 app.use(express.json());
 app.use(cors());
 
--- Server/routes/route.js
+++ Server/routes/route.js
@@ -2,9 +2,6 @@ const express = require("express");
 const Router = express.Router();
 const db = require("../config/db");
 const convertTextToSQL = require("../middleware/geminAI");
-// const limiter = require("../middleware/retelimit");
-// const NodeCache = require("node-cache");
-// const queryCache = new NodeCache({ stdTTL: 300 });
 
 Router.post("/query", async (req, res) => {
   const { naturalQuery } = req.body;

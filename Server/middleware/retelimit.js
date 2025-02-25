2efe7900d36ff9883bde566ffb182991aa2956e0
Isjupriya Rath <ishupriyarath@gmail.com>
2025-02-25 11:38:46 +0530

    updating

--- Server/middleware/retelimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 5,
-  message: { error: "Too many requests. Please try again later." },
-});
-
-module.exports = limiter;
